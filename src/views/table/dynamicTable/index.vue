f6a33ff7ca68b8bf7100ab7ab8c92d910ad11897
liangkuaiqianderoubaozi <360788929@qq.com>
2019-03-19 17:58:32 +0800

    修改admin项目

---
+++ src/views/table/dynamicTable/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="app-container">
+    <div style="margin:0 0 5px 20px">{{ table.dynamicTips1 }}</div>
+    <fixed-thead />
+
+    <div style="margin:30px 0 5px 20px">{{ table.dynamicTips2 }}</div>
+    <unfixed-thead />
+  </div>
+</template>
+
+<script>
+import fixedThead from './fixedThead'
+import unfixedThead from './unfixedThead'
+
+export default {
+  name: 'DynamicTable',
+  components: { fixedThead, unfixedThead },
+  data() {
+    return {
+      table: {
+        dynamicTips1: '固定表头, 按照表头顺序排序',
+        dynamicTips2: '不固定表头, 按照点击顺序排序',
+        dragTips1: '默认顺序',
+        dragTips2: '拖拽后顺序',
+        title: '标题',
+        importance: '重要性',
+        type: '类型',
+        remark: '点评',
+        search: '搜索',
+        add: '添加',
+        export: '导出',
+        reviewer: '审核人',
+        id: '序号',
+        date: '时间',
+        author: '作者',
+        readings: '阅读数',
+        status: '状态',
+        actions: '操作',
+        edit: '编辑',
+        publish: '发布',
+        draft: '草稿',
+        delete: '删除',
+        cancel: '取 消',
+        confirm: '确 定'
+      }
+    }
+  }
+}
+</script>
+
