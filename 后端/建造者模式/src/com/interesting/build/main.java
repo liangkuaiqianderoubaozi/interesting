1969273d26cc1b90c4d9cb532532f8c8b1de2603
liangkuaiqianderoubaozi <360788929@qq.com>
2019-06-25 09:44:18 +0800

    进制转换+位运算+rgb

--- 后端/建造者模式/src/com/interesting/build/main.java
+++ 后端/建造者模式/src/com/interesting/build/main.java
@@ -6,16 +6,21 @@ import com.interesting.build.builder.MealBuilder;
 public class main {
 
     public static void main(String[] args) {
-        MealBuilder mealBuilder = new MealBuilder();
 
-        Meal vegMeal = mealBuilder.prepareVegMeal();
-        System.out.println("Veg Meal");
-        vegMeal.showItems();
-        System.out.println("Total Cost: " +vegMeal.getCost());
-
-        Meal nonVegMeal = mealBuilder.prepareNonVegMeal();
-        System.out.println("\n\nNon-Veg Meal");
-        nonVegMeal.showItems();
-        System.out.println("Total Cost: " +nonVegMeal.getCost());
+        int a = 16 >> 1;
+        System.out.println(a);
+//        MealBuilder mealBuilder = new MealBuilder();
+//
+//        Meal vegMeal = mealBuilder.prepareVegMeal();
+//        System.out.println("Veg Meal");
+//        vegMeal.showItems();
+//        System.out.println("Total Cost: " +vegMeal.getCost());
+//
+//        Meal nonVegMeal = mealBuilder.prepareNonVegMeal();
+//        System.out.println("\n\nNon-Veg Meal");
+//        nonVegMeal.showItems();
+//        System.out.println("Total Cost: " +nonVegMeal.getCost());
     }
+
+
 }
