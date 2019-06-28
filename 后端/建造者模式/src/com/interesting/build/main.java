6fb8b21db0d49d8e01f8eb6928784b273bd62c41
liangkuaiqianderoubaozi <360788929@qq.com>
2019-06-28 17:59:45 +0800

    图片修改+排序

--- 后端/建造者模式/src/com/interesting/build/main.java
+++ 后端/建造者模式/src/com/interesting/build/main.java
@@ -1,25 +1,32 @@
 package com.interesting.build;
 
-import com.interesting.build.builder.Meal;
-import com.interesting.build.builder.MealBuilder;
+
+import java.util.*;
 
 public class main {
+
 
     public static void main(String[] args) {
 
         int a = 16 >> 1;
         System.out.println(a);
-//        MealBuilder mealBuilder = new MealBuilder();
-//
-//        Meal vegMeal = mealBuilder.prepareVegMeal();
-//        System.out.println("Veg Meal");
-//        vegMeal.showItems();
-//        System.out.println("Total Cost: " +vegMeal.getCost());
-//
-//        Meal nonVegMeal = mealBuilder.prepareNonVegMeal();
-//        System.out.println("\n\nNon-Veg Meal");
-//        nonVegMeal.showItems();
-//        System.out.println("Total Cost: " +nonVegMeal.getCost());
+
+
+        ArrayList<Integer> list = new ArrayList<>(3);
+        list.add(5);
+        list.add(3);
+        list.add(8);
+
+        Collections.sort(list);
+        System.out.println(list);
+
+        TreeMap<Integer, String> map = new TreeMap<>();
+        map.put(8, "3");
+        map.put(3, "1222");
+        map.put(4, "533");
+
+        System.out.println(map.get(list.get(0)));
+
     }
 
 
